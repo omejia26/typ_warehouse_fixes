9589fa84334ff9688aa00c135bd51bec8052c480
omejia26 <omejia@typrefrigeracion.com>
2016-06-01 08:50:37 -0700

    [ADD] typ_warehouse_fixes: agregamos importacion y definicion del modulo

---
+++ __openerp__.py
@@ -0,0 +1,19 @@
+# -*- encoding: utf-8 -*-
+{
+	'name': 'TyP Warehouse fixes',
+	'version': '0.1',
+	'author': 'Omar Mejia',
+	'category': 'TyP',
+	'description': """
+		Este modulo, hace cambios en stock
+		- Agrega filtro y campo calculado de Ubicaciones
+	""",
+	'website': 'www.typrefrigeracion.com.mx',
+	'depends': [
+		'stock',
+	],
+	'data': [
+		'stock_move.xml',
+	],
+	'installable': True
+}
